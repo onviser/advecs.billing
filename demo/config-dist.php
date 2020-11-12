0e29024d001bc7abfb72ef0c69ce51bd0fca59e6
Sergey <sergey@onviser.com>
2020-11-12 16:14:25 +0300

    BAC-8, fix

---
+++ demo/config-dist.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+return [
+    'mysql' => [
+        'host'     => 'localhost',
+        'user'     => 'user',
+        'password' => 'password',
+        'database' => 'billing',
+        'port'     => 3306,
+    ]
+];
