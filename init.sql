a70dd23576f7a127f336643219cd097115cbe619
Sergey <sergey@onviser.com>
2020-12-27 00:21:46 +0300

    обработка уведомлений от ПСКБ

--- init.sql
+++ init.sql
@@ -75,6 +75,7 @@ CREATE TABLE billing_pscb_payment
     payment_update  INTEGER UNSIGNED DEFAULT 0,
     payment_json    TEXT
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 20
   DEFAULT CHARSET = utf8 COMMENT 'ПСКБ, платежи';
 ALTER TABLE billing_pscb_payment
     ADD INDEX (id_account);
@@ -93,5 +94,5 @@ CREATE TABLE billing_pscb_notify
     notify_status TINYINT UNSIGNED DEFAULT 1,
     notify_add    INTEGER UNSIGNED DEFAULT 0
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 1000
+  AUTO_INCREMENT = 20
   DEFAULT CHARSET = utf8 COMMENT 'ПСКБ, уведомления от банка';
