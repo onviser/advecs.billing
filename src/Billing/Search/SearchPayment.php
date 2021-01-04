8356148f543e6e3d6f74d4bd6aa96b17e691ae27
Sergey <sergey@onviser.com>
2021-01-04 17:31:04 +0300

    поиск платежей

--- src/Billing/Search/SearchPayment.php
+++ src/Billing/Search/SearchPayment.php
@@ -13,6 +13,18 @@ class SearchPayment extends AbstractSearch
     protected $id = 0;
 
     /** @return int */
+    protected $timeFrom = 0;
+
+    /** @return int */
+    protected $timeTo = 0;
+
+    /** @return float */
+    protected $amountFrom = 0.0;
+
+    /** @return float */
+    protected $amountTo = 0.0;
+
+    /** @return int */
     protected $paymentStatus = 0;
 
     /**
@@ -29,6 +41,54 @@ class SearchPayment extends AbstractSearch
     public function getId(): int
     {
         return $this->id;
+    }
+
+    /**
+     * @param int $timeFrom
+     * @param int $timeTo
+     * @return $this
+     */
+    public function setTime(int $timeFrom = 0, int $timeTo = 0): self
+    {
+        $this->timeFrom = $timeFrom;
+        $this->timeTo = $timeTo;
+        return $this;
+    }
+
+    /** @return int */
+    public function getTimeFrom(): int
+    {
+        return $this->timeFrom;
+    }
+
+    /** @return int */
+    public function getTimeTo(): int
+    {
+        return $this->timeTo;
+    }
+
+    /**
+     * @param float $amountFrom
+     * @param float $amountTo
+     * @return $this
+     */
+    public function setAmount(float $amountFrom = 0.0, float $amountTo = 0.0): self
+    {
+        $this->amountFrom = $amountFrom;
+        $this->amountTo = $amountTo;
+        return $this;
+    }
+
+    /** @return float */
+    public function getAmountFrom(): float
+    {
+        return $this->amountFrom;
+    }
+
+    /** @return float */
+    public function getAmountTo(): float
+    {
+        return $this->amountTo;
     }
 
     /**
